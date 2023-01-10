58abc1e5c9b4ee6f54056abe707f1b574e188d01
Dr0pyyyy <adamkoutny12345seznam.cz>
2023-01-10 18:51:51 +0100

    minorChanges

---
+++ GuitaristHelperUnitTests/SpotifyManagerTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GuitaristHelperUnitTests
+{
+    [TestClass]
+    public class SpotifyManagerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
